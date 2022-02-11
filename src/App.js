8f8fe825e6982ff82966c4b41cebc328aa0c4e2f
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-02-11 15:05:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [login, setLogin] = useState('Wax Now!')
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{login}</h1>
+        <div className="buttons">
+          <button onClick={() => setLogin('Currently Waxing!')}>Wax On</button>
+          <button onClick={() => setLogin('Wax Now!')}>Wax Off</button>
+        </div>
       </header>
     </div>
   );
